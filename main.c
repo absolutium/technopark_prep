6a65903d370acac2146265e1313683d0f560e4d9
Alexey <32223355+absolutium@users.noreply.github.com>
2017-10-08 14:42:18 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -14,40 +14,73 @@ void freeMatrix(int row, int col, double **matrix)
 
 void printMatrix(int row, int col, double **matrix)
 {
-    printf("Rows: %d\n", row);
+    printf("\nRows: %d\n", row);
     printf("Columns: %d\n", col);
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
-            printf("%lf ", matrix[i][j]);
+            printf("%.2lf ", matrix[i][j]);
         printf("\n");
     }
+    printf("\n");
+
 }
 
-/*void multiply(int a)
+double **get_matrix(FILE *file, int row, int col)       //создает матрицу из файла
 {
-    double **resultMatrix = (double**) malloc((aRow * bCol) * sizeof(double*));
+    double **matrix = (double**) malloc((row * col) * sizeof(double*));
+    for (int i = 0; i < (row * col); i++)
+    {
+        matrix[i] = (double*) malloc((row * col) * sizeof(double));
+    }
+
+    double buff = 0.0;
+
+    while (!feof(file))
+    {
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+
+                fscanf(file, "%lf" , &buff);
+                if(buff > (sizeof(double)-1))
+                {
+                    printf("\nElement is too big!\n");
+                    exit(1);
+                }
+                matrix[i][j] = buff;
+            }
+        }
+    }
+    return matrix;
+}
+
+double **multiply(double **amatrix, double **bmatrix, int aRow, int bCol, int aCol)     //возвращает матрицу, являющуюся результатом умножения amatrix и bmatrix
+{
+    double **cmatrix = (double**) malloc((aRow * bCol) * sizeof(double*));
     for (int i = 0; i < (aRow * bCol); i++)
     {
-        resultMatrix[i] = (int*) malloc((aRow * bCol) * sizeof(int));
+        cmatrix[i] = (int*) malloc((aRow * bCol) * sizeof(int));
     }
 
     for(int i = 0; i < aRow; i++)
     {
         for(int j = 0; j < bCol; j++)
         {
-            resultMatrix[i][j] = 0;
+            cmatrix[i][j] = 0;
             for(int k = 0; k < aCol; k++)
             {
-                resultMatrix[i][j] += aMatrix[i][k] * bMatrix[k][j];
+                cmatrix[i][j] += amatrix[i][k] * bmatrix[k][j];
             }
         }
     }
-}*/
+    return cmatrix;
+}
 
 #define MAX_PATH_SIZE 100
 
-int main(int argc, char* argv[])
+int main()
 {
     char filePath[MAX_PATH_SIZE];
     int N = 0;
@@ -55,8 +88,20 @@ int main(int argc, char* argv[])
     printf("Enter amount of files: ");
     scanf("%d", &N);
 
-    int row = 0;
-    int col = 0;
+    if(N <= 1)
+    {
+        printf("\nNothing to multiply! Press enter to exit...\n");
+        exit(0);
+    }
+
+    double **amatrix = NULL;
+    double **bmatrix = NULL;
+    double **cmatrix = NULL;
+
+    int aRow = 0;       //количество строк для матрицы А (то же самое для матрицы В)
+    int aCol = 0;       //количество столбцов для матрицы А (то же самое для матрицы В)
+    int bRow = 0;
+    int bCol = 0;
 
     for (int i = 0; i < N; i++)
     {
@@ -71,52 +116,35 @@ int main(int argc, char* argv[])
             exit(1);
         }
 
-        int row = 0;       //количество строк в матрице
-        int col = 0;       //количество столбцов в матрице
-
-        double buff = 0;       //буфер чисел в файле
-
-        fscanf(file, "%d", &row);
-        buff = 0;
-
-        fscanf(file, "%d", &col);
-        buff = 0;
-
-        double **matrix = (double**) malloc((row * col) * sizeof(double*));
-        for (int i = 0; i < (row * col); i++)
-        {
-            matrix[i] = (double*) malloc((row * col) * sizeof(double));
-        }
-
-        while (!feof(file))
-        {
-            for(int i = 0; i < row; i++)
-            {
-                for(int j = 0; j < col; j++)
-                {
-
-                    fscanf(file, "%lf" , &buff);
-                    if(buff > (sizeof(double)-1))
-                    {
-                        printf("\nElement is too big!\n");
-                        exit(1);
-                    }
-                    matrix[i][j] = buff;
-                }
-            }
-        }
-
-        /*switch (i) {
+        switch (i) {
         case 0:
-            memcpy(amatrix, matrix, sizeof(matrix));
+            fscanf(file, "%d", &aRow);
+            fscanf(file, "%d", &aCol);
+            amatrix = get_matrix(file, aRow, aCol);
             break;
         default:
-            memcpy(bmatrix, matrix, sizeof(matrix));
-            multiply(amatrix, bmatrix);
-            memcpy(amatrix, cmatrix, sizeof(cmatrix));
+            fscanf(file, "%d", &bRow);
+            fscanf(file, "%d", &bCol);
+
+            if(aCol != bRow)    //проверка возможности умножения
+            {
+                printf("\nCan't multiply!\n");
+                exit(1);
+            }
+
+            bmatrix = get_matrix(file, bRow, bCol);
+            cmatrix = multiply(amatrix, bmatrix, aRow, bCol, aCol);
+            amatrix = cmatrix;
             break;
-        }*/
-        printMatrix(row, col, matrix);
+        }
+
+        printMatrix(aRow, aCol, amatrix);
         fclose(file);
     }
+    printf("RESULT: \n");
+    printMatrix(aRow, bCol, cmatrix);
+
+    freeMatrix(aRow, aCol, amatrix);
+    freeMatrix(bRow, bCol, bmatrix);
+    freeMatrix(aRow, bCol, cmatrix);
 }
